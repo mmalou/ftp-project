293a8eb9361dbad269595c9ef0cd27fce6e395c4
Julien Riffard <jriffarddev@gmail.com>
2014-12-10 17:39:40 +0100

    server ftp alpha. username = john / password = bar

--- Server/app.js
+++ Server/app.js
@@ -24,7 +24,7 @@ if (cluster.isMaster) {
 // Code to run if we're in a worker process
 } else {
 
-    // Include Express
+    /*// Include Express
     var express = require('express');
 
     // Create a new Express application
@@ -38,7 +38,46 @@ if (cluster.isMaster) {
     });
 
     // Bind to a port
-    app.listen(3000);
+    app.listen(3000);*/
+
+    var ftpd = require('ftpd');
+    var fs = require('fs');
+
+    var options = {
+      pasvPortRangeStart: 4000,
+      pasvPortRangeEnd: 5000,
+      getInitialCwd: function(connection, callback) {
+        var userPath = '/Users/Julien/Downloads';//process.cwd() + '/' + connection.username;
+        fs.exists(userPath, function(exists) {
+          exists ? callback(null, userPath) : callback('path does not exist', userPath);
+        });
+      },
+      getRoot: function(user) {
+        return '/';
+      }
+    };
+
+    var host = '127.0.0.1';
+
+    var server = new ftpd.FtpServer(host, options);
+
+    server.on('client:connected', function(conn) {
+      var username;
+      console.log('Client connected from ' + conn.socket.remoteAddress);
+      conn.on('command:user', function(user, success, failure) {
+        username = user;
+        (user == 'john') ? success() : failure();
+      });
+      conn.on('command:pass', function(pass, success, failure) {
+        // check the password
+        (pass == 'bar') ? success(username) : failure();
+      });
+    });
+
+    server.listen(21);
+    console.log('FTPD listening on port 21');
+
+
     console.log('Worker ' + cluster.worker.id + ' running!');
 
 }
